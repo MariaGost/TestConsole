57e59a5def853ca87af44cdf0c5a74333b354636
Tahir <tahir.khairullin@gmail.com>
2020-02-13 15:26:45 +0300

    Init

---
+++ TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+            Console.Write("Введите >");
+            string message = Console.ReadLine();
+
+            Console.WriteLine("Введено: " + message);
+            Console.ReadLine();
+        }
+    }
+}
